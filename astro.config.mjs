d84eb53b36bfe8ec30dbaf07b8f52e6937aa9540
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2024-05-09 01:42:35 +0200

    install Sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
 import { defineConfig } from 'astro/config';
+import sanityIntegration from "@sanity/astro";
+import react from "@astrojs/react";
+import { loadEnv } from "vite";
+import netlify from "@astrojs/netlify";
+
+const { PUBLIC_SANITY_STUDIO_PROJECT_ID, PUBLIC_SANITY_STUDIO_DATASET } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'hybrid',
+    integrations: [
+        sanityIntegration({
+            projectId: PUBLIC_SANITY_STUDIO_PROJECT_ID,
+            dataset: PUBLIC_SANITY_STUDIO_DATASET,
+            useCdn: true,
+            studioBasePath: '/admin',
+        }),
+        react(),
+    ],
+    adapter: netlify(),
+});
